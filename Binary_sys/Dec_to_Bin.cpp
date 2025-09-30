82eea63a28adb24d42efdc78d2898173ae07a68b
himash-design <bhimanshi988@gmail.com>
2025-09-30 14:11:34 +0530

    Initial commit with DSA practice problems

---
+++ Binary_sys/Dec_to_Bin.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int dectoBin (int decNum){
+    int ans = 0 , pow=1;
+
+    while (decNum >0)
+{
+    int rem = decNum % 2;
+    decNum/= 2;
+    ans+= (rem * pow);
+    pow*= 10 ;
+
+}
+
+  return ans; // Binary Form
+}
+
+int bintoDec(int binNum){
+    int ans = 0 , pow =1;
+
+    while(binNum > 0){
+        int rem = binNum%10;
+        ans =0;
+    }
+}
+
+int main() {
+    int decNum= 7; 
+    cout << dectoBin(decNum) << endl;
+   
+    }
+
+
+    
